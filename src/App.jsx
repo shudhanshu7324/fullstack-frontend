79782667163a541ff8f11a714b37a527e55c4eea
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-06-12 16:04:36 +0530

    frontend backend connected

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react"
+import axios from 'axios'
+
+
+function App() {
+  
+  const [jokes,setJokes] = useState([]);
+
+  useEffect(()=>{
+    axios.get('http://localhost:3001/getJokes').
+      then((response)=>setJokes(response.data)).
+      catch((error)=>console.log(error))
+  },[]);
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>Jokes: {jokes.length}</p>
+      {
+        jokes.map((joke)=>{
+          <div key={joke.id}>
+            <h3>{joke.title}</h3>
+            <p>{joke.content}</p>
+          </div>
+        })
+      }
+    </>
+  )
+}
+
+export default App
